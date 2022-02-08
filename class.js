6fe5c4170ff0ce0beff44b81f3f035193ce39535
Firoz-Alam <51852342+Firoz-Alam@users.noreply.github.com>
2022-02-08 16:27:14 +0600

    class

---
+++ class.js
@@ -0,0 +1,14 @@
+class Student{
+    constructor(id,name){
+        this.id = id;
+        this.name = name;
+    }
+}
+
+const student1 = new Student(2,"Firoz");
+const student2 = new Student(3,"Alam");
+const student3 = new Student(4,"Rakib");
+
+console.log(student1.id,student1.name);
+console.log(student2);
+console.log(student3);
